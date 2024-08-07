c9e7e6630d37b71e6e4c262611bc23d216b4a4e7
GhulamHussainJafari <114880754+GhulamHussainJafari@users.noreply.github.com>
2024-08-07 09:49:56 -0700

    finish work1

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const ContentSecurityPolicy = require('./csp')
 const redirects = require('./redirects')
 
 const nextConfig = {
+  typescript: {
+    ignoreBuildError: true,
+  },
   reactStrictMode: true,
   swcMinify: true,
   images: {
